c84c6e39214a2f77497f4e34b8fea78bc273db4d
Luis Costa <felipeeusoudoscosta@gmail.com>
2026-05-19 12:52:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import ipaddress
 
+ip = input("Digite um endereço IP com máscara (ex: 192.168.1.1/24): ")
+
+try:
+    network = ipaddress.ip_network(ip, strict=False)
+
+    print(f"\nRede: {network.network_address}")
+    print(f"Broadcast: {network.broadcast_address}")
+    print(f"Máscara: {network.netmask}")
+    print(f"Quantidade de hosts: {network.num_addresses - 2}")
+
+except ValueError:
+    print("IP inválido.")
